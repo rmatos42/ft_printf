f50410dd715c857cbcb81a68d9748f1c76dafd57
Ryley Matos <rmatos@e1z3r1p5.42.us.org>
2017-04-17 18:28:19 -0700

    may have fixed something

--- print_char.c
+++ print_char.c
@@ -5,10 +5,7 @@ t_ret	print_char(va_list	args, t_mod mod)
 	t_ret	ret;
 	char	c;
 
-	if (mod.l)
-		return print_wchar(args, mod);
-	else
-		c = va_arg(args, int);
+	c = va_arg(args, int);
 	ret.str = ft_memalloc(sizeof(char) * 2);
 	ret.str[0] = c;
 	ret.str[1] = '\0';
