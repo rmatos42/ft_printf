703bcf4d631b0e18a776aebcc535479e3c0fe312
Ryley Matos <rmatos@e1z3r1p5.42.us.org>
2017-04-17 18:29:33 -0700

    fixie

--- print_wide.c
+++ print_wide.c
@@ -2,11 +2,7 @@
 
 t_ret	print_wstr(va_list	args, t_mod mod)
 {
-	t_ret ret;
-
-	ret.str = "@@";
-	ret.len = 2;
-	return (ret);
+	return print_str(args, mod);
 }
 
 t_ret	print_wchar(va_list	args, t_mod mod)
