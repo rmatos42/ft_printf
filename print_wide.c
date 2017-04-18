7e42785f1863a30ea392d9d5aa18b10143ce3352
Ryley Matos <rmatos@e1z3r1p5.42.us.org>
2017-04-17 18:25:57 -0700

    fixed wide for mouli

--- print_wide.c
+++ print_wide.c
@@ -11,11 +11,5 @@ t_ret	print_wstr(va_list	args, t_mod mod)
 
 t_ret	print_wchar(va_list	args, t_mod mod)
 {
-	t_ret ret;
-	wchar_t	w;
-
-	w = va_arg(args, wchar_t);
-	ret.str = "�";
-	ret.len = 4;
-	return (ret);
+	return (print_char(args, mod));
 }
